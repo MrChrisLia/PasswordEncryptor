a870f67a8d5ff701c4f2fe95f65d63b791e70163
MrChrisLia <chrislia92@gmail.com>
2022-03-12 21:23:47 +0800

    added jira authentication

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-import cryptocode
+from wsgiref import headers
 
-def encryptor(password,username):
+import cryptocode
+import sqlite3
+import requests
+import base64
+
+# To do: Create SQL DB
+
+# Basic Jira Authentication
+auth_key = base64.b64encode(b"EMAIL:APIKEY").decode("ascii")
+header = {"Authorization": "Basic " + auth_key}
+response = requests.get("https://asiasupport247.atlassian.net/", headers=header)
+print(response.status_code)
+
+
+# To do: Create a login GUI. If status code is 200, it means you can authenticate to Atlassian,
+# thus unlocking the passwords. If status code isn't 200, it means you can't authenticate to Atlassian,
+# thus cannot unlock the passwords
+
+
+
+def encryptor(password, username):
   str_encoded = cryptocode.encrypt(password, username)
   print('Success!')
   print(str_encoded)
@@ -13,5 +33,6 @@ def decryptor(password, username):
   str_decoded = cryptocode.decrypt(password, username)
   print('Success!')
   print(str_decoded)
+
 
 decryptor('sjoQiw==*6ZimfHrY9HUxCySi2hzkKA==*vDCkTKVgOK0ukyQyXYq3hg==*1BuhkAfKx7Qf2AmQDeUj4A==', 'itoc')
